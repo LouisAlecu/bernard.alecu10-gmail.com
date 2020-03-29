f51aedd68c32f1271bc95983133e498f8cce0f8b
Bernard Louis Alecu <bernard.alecu10@gmail.com>
2020-03-29 21:19:47 +0100

    Progress

--- sp_py/api_handler.py
+++ sp_py/api_handler.py
@@ -1,8 +1,9 @@
 import requests
 import logging
 
+
 class ApiClient:
-    def __init__(self, root_endpoint, version, api_token = None):
+    def __init__(self, root_endpoint, version, api_token=None):
         """
         Initialize the attributes based on the api information
         """
@@ -12,8 +13,9 @@ class ApiClient:
         self._response = None
         self._data = {}
         self._last_req_status = None
+        self._api_limit = 50
 
-        if (self._api_token):
+        if self._api_token:
             self._is_authenticated = True
         else:
             self._is_authenticated = False
@@ -22,27 +24,27 @@ class ApiClient:
         """ 
         GET request to the /companies/search endpoint
         """
-        url = self._url('companies', 'search')
+        url = self._url("companies", "search")
         print(url)
-        if 'api_token' not in params:
-            params['api_token'] = self._api_token
+        if "api_token" not in params:
+            params["api_token"] = self._api_token
         self._response = requests.get(url, params=params)
-        print("request status: ", self._response.status_code)
-        self._data = self._response.json()
         self._last_req_status = self._response.status_code
+        if self._error_handling():
+            self._data = self._response.json()
 
     def GET_company(self, jurisdiction_code, company_number, params={}):
         """ 
         GET request to the /companies/:jurisdiction:/:company_number: endpoint
         """
-        url = self._url('companies', jurisdiction_code, company_number)
+        url = self._url("companies", jurisdiction_code, company_number)
         print(url)
-        if 'api_token' not in params:
-            params['api_token'] = self._api_token
+        if "api_token" not in params:
+            params["api_token"] = self._api_token
         self._response = requests.get(url, params=params)
-        print("request status: ", self._response.status_code)
-        self._data = self._response.json()
         self._last_req_status = self._response.status_code
+        if self._error_handling():
+            self._data = self._response.json()
 
     def GET_custom_endpoint(self, *args, params={}):
         """
@@ -51,35 +53,62 @@ class ApiClient:
         """
         url = self._url(*args)
         print(url)
-        if 'api_token' not in params:
-            params['api_token'] = self._api_token
-        print(params['api_token'])
+        if "api_token" not in params:
+            params["api_token"] = self._api_token
+        print(params["api_token"])
         self._response = requests.get(url, params=params)
-        print("request status: ", self._response.status_code)
-        self._last_req_status = self._response.status_code        
+        self._last_req_status = self._response.status_code
+        self._error_handling()
 
-    def get_json(self):
-        return self._data
+    def _error_handling(self):
+        if self._last_req_status == 200:
+            print("Request executed successfully.")
+            return True
+        elif self._last_req_status == 403:
+            print("You exceeded the requests limit.")
+        elif self._last_req_status == 401:
+            print("Unauthorized access. Probably invalid credentials.")
+        elif self._last_req_status == 400:
+            print("Bad request.")
+        else:
+            print("The response code is {self._last_req_status}")
+        return False
 
     def _url(self, *args):
-        return f"{self._root_endpoint}/{self._version}/{'/'.join(args)}" if args else f"{self._root_endpoint}"
-
-    def get_request_status(self):
-        return self._last_req_status
+        return (
+            f"{self._root_endpoint}/{self._version}/{'/'.join(args)}"
+            if args
+            else f"{self._root_endpoint}"
+        )
 
     def upload_api_token(self, api_token):
         """
         Functionality to change the api token.
         """
-        print("This will try a request with your token to check if it is valid, so the last request status will change.")
-        self.GET_custom_endpoint('companies', 'us_nj', '0400549703', 'events', params={'api_token': api_token})
-        if(self._last_req_status == 200):
+        print(
+            "This will try a request with your token to check if it is valid, so the last request status will change."
+        )
+        # The next is an endpoint that requires credentials.
+        self.GET_custom_endpoint(
+            "companies",
+            "us_nj",
+            "0400549703",
+            "events",
+            params={"api_token": api_token},
+        )
+        if self._last_req_status == 200:
             self._is_authenticated = True
         else:
-            print("Token might be invalid, code {self._last_req_status} returned. Check codes meaning in the documentation.")
+            print(
+                "Token might be invalid, code {self._last_req_status} returned. Check codes meaning in the documentation."
+            )
             self._is_authenticated = False
+
+    def get_request_status(self):
+        return self._last_req_status
+
+    def get_json(self):
+        return self._data
 
     def get_response_object_itself(self):
         return self._response
-
-
--- sp_py/test_api_handler.py
+++ sp_py/test_api_handler.py
@@ -12,6 +12,6 @@ def test_GET_company():
 def test_GET_companies_search():
     client = ApiClient("https://api.opencorporates.com", "v0.4", api_token=None)
     client.GET_company('pl', '0000011037')
-    data = client.get_json()["results"]["companies"]
+    data = client.get_json()
     
-    assert (client.get_request_response() == 200 and len(data) != 0)
+    assert (client.get_request_status() == 200 and len(data) != 0)
